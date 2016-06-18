08811d116ae62edd892dd7480af00376aebc2abb
Josh Trommel <joshtrommel@gmail.com>
2016-06-18 01:54:56 -0700

    Updated for PEP standards

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-import os, sys, operator
+import os
+import sys
+import operator
+
 import requests
 
+
 def log(m):
-	print('=> {}'.format(m))
+    print('=> {}'.format(m))
 
 base = 'http://www.omdbapi.com/?t={}'
 movies, errors, files = [], [], []
 
 try:
-	dir = sys.argv[1]
-	files = os.listdir(dir)
+    dir = sys.argv[1]
+    files = os.listdir(dir)
 except IndexError:
-	log('Error: No path to movies provided')
+    log('Error: No path to movies provided')
 
 for f in files:
-	f = f.replace(' ', '+').split('.', 1)[0]
-	url = base.format(f)
+    f = f.replace(' ', '+').split('.', 1)[0]
+    url = base.format(f)
 
-	r = requests.get(url)
+    r = requests.get(url)
 
-	if r.json().get('Response') == 'False':
-		errors.append('Failed attempt with "{}"'.format(f))
-	else:
-		title = r.json().get('Title')
-		rating = r.json().get('imdbRating')
-		movies.append({'title': title, 'rating': rating})
+    if r.json().get('Response') == 'False':
+        errors.append('Failed attempt with "{}"'.format(f))
+    else:
+        title = r.json().get('Title')
+        rating = r.json().get('imdbRating')
+        movies.append({'title': title, 'rating': rating})
 
 movies.sort(key=operator.itemgetter('rating'), reverse=True)
 
 for movie in movies:
-	log("{}: {}".format(movie['title'], movie['rating']))
+    log("{}: {}".format(movie['title'], movie['rating']))
 
 for error in errors:
-	log(error)
+    log(error)
