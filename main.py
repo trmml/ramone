f52239b747aa32cc34fd5c6c772ba5422db817b8
msafadieh <me@msafadieh.com>
2018-08-06 02:00:13 +0300

    replaced error catching with conditional

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 import sys
 import operator
-
 import requests
 
 base = 'http://www.omdbapi.com/?t={}'
 movies, errors, files = [], [], []
 
-try:
-    dir = sys.argv[1]
-    files = os.listdir(dir)
-except IndexError:
-    print('* Error: No path to movies provided')
+if len(sys.argv) < 1: 
+    exit('* Error: No path to movies provided')
+
+dir = sys.argv[1]
+files = os.listdir(dir)
 
 for f in files:
     f = f.replace(' ', '+').split('.', 1)[0]
