a4d1a8bbe6307c733bb72a3e530facc78481cd11
msafadieh <me@msafadieh.com>
2018-08-06 18:22:03 +0300

    fixed URL (key required) and string splitting

--- ramone.py
+++ ramone.py
@@ -2,7 +2,9 @@ import os, sys, operator, requests, re
 
 video_extensions = ['webm', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'ogg', 'mp4', 'm4p', 'm4v', 'mkv', 'mov', 'flv', 'avi']
 regular_exp = re.compile(r",|\.|-| ")
-base = 'http://www.omdbapi.com/?t={}'
+# you must obtain an API key from omdapi.com
+# example: api_url = '' http://www.omdbapi.com/?i=XXXXXXXXX&apikey=XXXXXXXX
+api_url = ''
 movies, errors = [], []
 
 # exits if no path is provided
@@ -25,7 +27,7 @@ for f in files:
         continue
     
     # creates and requests URL
-    url = base.format('+'.join(f[:-1]))
+    url = f"{api_url}&t={('+'.join(movie[:-1]))}"
     r = requests.get(url)
 
     # appends movie to correct list depending on response
